003c232bad3864ea45a2df4b90e843f187b1f3f8
Harish <144200799+harish2196@users.noreply.github.com>
2024-05-03 14:44:06 +0530

    Product Pricing App - JDBC

---
+++ dao/ProductDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.chainsys.arraylist1.Product;
+
+import model.UserRegister;
+
+public interface ProductDAO {
+	void updateProducts(Connection connection) throws SQLException ;
+	public  void insertProduct(Connection connection, Product p) throws SQLException;
+	public void insertUser(Connection connection,UserRegister ur)throws SQLException;
+	public void getUserPassword(Connection connection,UserRegister ur) throws SQLException;
+	public boolean isExistsId(Connection connection, int id) throws SQLException;
+	public  boolean isExistsName(Connection connection, String name)throws SQLException;
+	void  viewProducts(Connection connection)throws SQLException;
+	void retrieveProducts(Connection connection)throws SQLException;
+	void deleteProducts(Connection connection)throws SQLException;
+}
